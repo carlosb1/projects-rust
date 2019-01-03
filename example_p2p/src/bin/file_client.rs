ad58b1da0bd0a1320186874a521a7c836d6773c2
Carlos B <carlos.baezruiz@gmail.com>
2019-01-03 22:05:05 +0100

    Added reference to create strings

--- example_p2p/src/bin/file_client.rs
+++ example_p2p/src/bin/file_client.rs
@@ -7,23 +7,29 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+pub struct Node<'a> {
+    filename: &'a str,
+    sha_id: String,
+    raw_info: Vec<u8>,
+}
+
+impl<'a> Node<'a> {
+    fn new(filename: &str) -> Node {
+        //TODO add checkers
+        let mut fil = File::open(filename).unwrap();
+        let mut buffer = Vec::new();
+        fil.read_to_end(&mut buffer).unwrap();
+        let mut hasher = Sha1::new();
+        hasher.input(&mut buffer);
+        let hex = hasher.result_str();
+        // calculate sha1
+        Node {filename: filename, sha_id: hex, raw_info: buffer}
+    }
+}
 
 fn main () -> io::Result<()> {
-    // read file buffer
-    let filename = String::from("/home/carlosb/Desktop/paper-ton.pdf");
-    let mut fil = File::open(filename)?;
-    let mut buffer = Vec::new();
-    fil.read_to_end(&mut buffer)?;
-
-    // calculate sha1
-    let mut hasher = Sha1::new();
-    hasher.input_str("hello world");
-    let hex = hasher.result_str(); 
-    println!("my hash = {}", hex);
-    hasher.reset();
-    hasher.input(&mut buffer);
-    let hex = hasher.result_str();
-    println!("my hash2 = {}", hex);
+    let str_value = "/home/carlosb/Desktop/2018-12-21.png".to_string(); 
+    Node::new(&str_value);
 
     Ok(())
 }
