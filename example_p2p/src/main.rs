ed8bfc8cf1e7e90c5536efa8eea3b9a522888e55
Carlos B <carlos.baezruiz@gmail.com>
2019-01-08 23:19:39 +0100

    Added example to use a tokio server

--- example_p2p/src/main.rs
+++ example_p2p/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate serde_derive;
+
 extern crate tokio;
 extern crate tokio_codec;
 
@@ -6,6 +9,38 @@ use tokio::codec::Decoder;
 use tokio_codec::BytesCodec;
 use tokio::prelude::*;
 use tokio::net::TcpListener;
+
+extern crate serde;
+extern crate serde_json;
+
+
+pub struct MsgManager;
+
+impl MsgManager {
+    fn run(&self, message: Message) {
+        match message.operation.as_ref() {
+            "run" => println!("Running!!"),
+            _ => println!("Nothing to do"),
+        };
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    operation: String,
+}
+
+impl Message {
+    fn new(operation: String) -> Message {
+        Message {operation: operation} 
+    }
+    fn new_empty() -> Message  {
+        Message {operation: "".to_string()}
+    }
+    
+}
+    
 
 fn main() {
     let addr = "127.0.0.1:12345".parse().unwrap();
@@ -18,7 +53,14 @@ fn main() {
                 let (_writer, reader) = framed.split();
 
                 let handle_conn = reader.for_each(|bytes| {
-                    println!("bytes: {:?}", bytes);
+                    //println!("bytes: {:?}", bytes);
+                    let v: Vec<u8> =  bytes.iter().map(|b| *b).collect();
+                    let message = String::from_utf8(v).unwrap();
+                    println!("in text: {}", message);
+                    let msg: Message = match serde_json::from_str(&message)  {
+                            Err(..) =>   {println!("It was not parsed correctly"); Message::new_empty() },
+                            Ok(msg) => msg,
+                    };  
                     Ok(())
                 })
                 .and_then(|()| {
