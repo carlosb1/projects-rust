8ab2b757235b419d803bc2af0b79e48c41c27fe0
Carlos B <carlos.baezruiz@gmail.com>
2019-03-18 19:31:45 +0100

    Finished Arc example

--- p2p_example/src/main.rs
+++ p2p_example/src/main.rs
@@ -1,16 +1,22 @@
 use futures::prelude::*;
+use std::sync::Arc;
+
 use libp2p::{
     NetworkBehaviour,
     secio,
     tokio_codec::{FramedRead, LinesCodec}
 };
 
+#[derive(Clone)]
 struct NodeManager {
     pub addresses: Vec<String>,
 }
 impl NodeManager {
     fn new() -> NodeManager {
         NodeManager{addresses: Vec::new()}
+    }
+    fn say_hello(self) {
+        println!("Hello to: {:?}",self.addresses);   
     }
 }
 
@@ -19,7 +25,7 @@ struct MyBehaviour<TSubstream: libp2p::tokio_io::AsyncRead + libp2p::tokio_io::A
     floodsub: libp2p::floodsub::Floodsub<TSubstream>,
     mdns: libp2p::mdns::Mdns<TSubstream>,
     #[behaviour(ignore)]
-    node_manager: NodeManager,
+    node_manager: Arc<NodeManager>,
 }
 
 
@@ -44,18 +50,23 @@ impl<TSubstream: libp2p::tokio_io::AsyncRead + libp2p::tokio_io::AsyncWrite> lib
 }
 
 
-//TODO class to parse values
 impl<TSubstream: libp2p::tokio_io::AsyncRead + libp2p::tokio_io::AsyncWrite> libp2p::core::swarm::NetworkBehaviourEventProcess<libp2p::floodsub::FloodsubEvent> for MyBehaviour<TSubstream> {
     fn inject_event(&mut self, message: libp2p::floodsub::FloodsubEvent) {
         if let libp2p::floodsub::FloodsubEvent::Message(message) = message {
             println!("Received '{:?}' from {:?}", String::from_utf8_lossy(&message.data), message.source);
             println!("---> my bytes {:?}", message.source.to_base58());
             if !self.node_manager.addresses.contains(&message.source.to_base58()) {
-                self.node_manager.addresses.push(message.source.to_base58());
+                //(*self.node_manager).clone().say_hello();
+                let mut cloned = (*self.node_manager).addresses.clone();
+                cloned.push(message.source.to_base58());
+                //TODO Apply boxes!
+                //(*self.node_manager.addresses) = cloned;
+                //let values = (*self.node_manager.addresses);
             }
         }
     }
 }
+
 
 
 
@@ -70,13 +81,15 @@ fn main() {
 
     let floodsub_topic = libp2p::floodsub::TopicBuilder::new("chat").build();
 
-    
+    let  node_manager: NodeManager =  NodeManager::new();
+    let rc_node = Arc::new(node_manager);
+
     // lambda function to initialise values
     let mut swarm =  {
         let mut behaviour = MyBehaviour {
             floodsub: libp2p::floodsub::Floodsub::new(local_peer_id.clone()),
             mdns: libp2p::mdns::Mdns::new().expect("Failed to create mDNS service"),
-            node_manager: NodeManager::new(),
+            node_manager: rc_node.clone(),
         };
         behaviour.floodsub.subscribe(floodsub_topic.clone());
         libp2p::Swarm::new(transport, behaviour, local_peer_id)
@@ -106,7 +119,10 @@ fn main() {
     tokio::run(futures::future::poll_fn(move || -> Result<_, ()> {
             loop {
                 match framed_stdin.poll().expect("Error while polling stdin") {
-                    Async::Ready(Some(line)) => swarm.floodsub.publish(&floodsub_topic, line.as_bytes()),
+                    Async::Ready(Some(line)) => {
+                        (*rc_node).clone().say_hello();
+                        swarm.floodsub.publish(&floodsub_topic, line.as_bytes())
+                    },
                     Async::Ready(None) => panic!("Stdin closed"),
                     Async::NotReady => break,
                 };
