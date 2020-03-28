464f24d29f02d2373867f87583ac8a0b23f8016c
carlosb <carlos.baezruiz@gmail.com>
2020-03-28 20:24:47 +0100

    Added example for db

---
+++ web_endpoint/app/src/client_db.rs
@@ -0,0 +1,23 @@
+#![feature(rustc_private)]
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate serde_derive;
+extern crate mongodb;
+extern crate rocket_contrib;
+extern crate serde_json;
+
+mod db;
+mod entities;
+
+use crate::db::{UsersRepository};
+use crate::entities::User;
+
+fn main() {
+    let _host = "0.0.0.0".to_string();
+    let _port = 27017;
+    let user_repository = UsersRepository::new(_host, _port);
+    user_repository.clone().create(User{idname: "idname".to_string(), idaddress:"idaddress".to_string()});
+    let restored_user = user_repository.clone().get("idname".to_string());
+    println!("{restored_user}", restored_user=restored_user.unwrap());
+
+    
+}
