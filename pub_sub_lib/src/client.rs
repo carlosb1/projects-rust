395d37539e8ae681e68de0a1175ef772f5d44c56
carlosb <carlos.baezruiz@gmail.com>
2020-04-06 21:43:21 +0200

    Added changes to split resps client and lib

--- pub_sub_lib/src/client.rs
+++ pub_sub_lib/src/client.rs
@@ -1,46 +1,10 @@
 
 
 //type ClientTransport = Framed<TcpStream, MyBytesCodec>;
-use tokio_util::codec::{FramedWrite, FramedRead};
-use tokio::net::TcpStream;
-use std::net::SocketAddr;
-use std::error::Error;
-use futures::{SinkExt, StreamExt};
-use pub_sub::{MyBytesCodec, JSONParser};
+use pub_sub::{Message, send};
 use futures::executor::block_on;
 use std::env;
 
-
-pub async fn run(address: String) -> Result<(), Box<dyn Error>> {
-    println!("Trying to connect to {}", address);
-    let remote_address: SocketAddr = address.parse().unwrap();
-    let mut tcp = TcpStream::connect(&remote_address).await?;
-    let (r, w) = tcp.split();
-    
-    let mut framed_writer = FramedWrite::new(w, MyBytesCodec{});
-    let mut framed_reader = FramedRead::new(r, MyBytesCodec{});
-    
-    let encoded: Vec<u8> = vec![1,1];
-    framed_writer.send(encoded).await?;
-
-    if let Some(frame) = framed_reader.next().await {
-        match frame {
-            Ok(message) => {
-                let json_parser = JSONParser::new();
-                println!("{:?}", message);
-                json_parser.parse(&message);
-                let resp: Vec<u8>  = vec![1,2];
-                framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
-            }
-            Err(e) => {
-                println!("Error received while we are reading {}", e);
-            }
-
-        }
-    }
-
-    Ok(()) 
-}
 
 pub fn main () {
     let args: Vec<String> = env::args().collect();
@@ -48,7 +12,7 @@ pub fn main () {
     if args.len() >= 2 {
         address = args[0].clone();   
     }
-    let future = run(address);
+    let future = send(address, Message::new("first_oper".to_string()));
     block_on(future);
 }
 
--- pub_sub_lib/src/lib.rs
+++ pub_sub_lib/src/lib.rs
@@ -10,6 +10,14 @@ extern crate serde_json;
 use bytes::BytesMut;
 use std::io;
 use tokio_util::codec::{Encoder,Decoder};
+use tokio_util::codec::{FramedWrite, FramedRead};
+use tokio::net::TcpStream;
+use std::net::SocketAddr;
+use std::error::Error;
+use futures::{SinkExt, StreamExt};
+use std::collections::HashMap;
+
+
 
 #[derive(Clone,Copy)]
 pub struct JSONParser;
@@ -34,6 +42,31 @@ pub struct Message {
     operation: String,
 }
 
+impl Message {
+    pub fn new(operation: String) -> Message {
+        Message{operation: operation}
+    }
+
+    pub fn ack() -> Message {
+        Message{operation: "ack".to_string()}
+    }
+
+    pub fn nack() -> Message {
+        Message{operation: "nack".to_string()}
+    }
+
+    pub fn login(channel: String, users: HashMap<&str,Vec<&str>>) -> Message {
+        Message{operation: "login".to_string()}
+    }
+    pub fn ack_login(users: HashMap<&str,Vec<&str>>) -> Message {
+        Message {operation: "ack_login".to_string()}
+    }
+
+    pub fn send_msg(msg: String) -> Message {
+        Message {operation: "send".to_string()} 
+    }
+}
+
 pub struct MyBytesCodec;
 
 
@@ -52,6 +85,7 @@ impl Decoder for MyBytesCodec {
     }
 }
 
+
 impl Encoder for MyBytesCodec {
     type Item = Vec<u8>;
     type Error = io::Error;
@@ -60,4 +94,34 @@ impl Encoder for MyBytesCodec {
         buf.extend(data);
         Ok(())
     }
+}
+
+pub async fn send(address: String, message: Message) -> Result<(), Box<dyn Error>> {
+    println!("Trying to connect to {}", address);
+    let remote_address: SocketAddr = address.parse().unwrap();
+    let mut tcp = TcpStream::connect(&remote_address).await?;
+    let (r, w) = tcp.split();
+    
+    let mut framed_writer = FramedWrite::new(w, MyBytesCodec{});
+    let mut framed_reader = FramedRead::new(r, MyBytesCodec{});
+    
+    let encoded: Vec<u8> = serde_json::to_vec(&message).unwrap();
+    framed_writer.send(encoded).await?;
+
+    if let Some(frame) = framed_reader.next().await {
+        match frame {
+            Ok(response) => {
+                let json_parser = JSONParser::new();
+                //println!("{:?}", response);
+                json_parser.parse(&response);
+                let resp: Vec<u8>  = vec![1,2];
+                framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
+            }
+            Err(e) => {
+                println!("Error received while we are reading {}", e);
+            }
+
+        }
+    }
+    Ok(()) 
 }
