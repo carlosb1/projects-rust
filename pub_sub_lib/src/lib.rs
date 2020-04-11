cf543fce19864e3ea6816dddd188b1b86c853fdb
carlosb <carlos.baezruiz@gmail.com>
2020-04-11 23:07:42 +0200

    Moved server class

--- pub_sub_lib/src/lib.rs
+++ pub_sub_lib/src/lib.rs
@@ -9,6 +9,7 @@ extern crate serde_json;
 
 use bytes::BytesMut;
 use std::io;
+use tokio::net::TcpListener;
 use tokio_util::codec::{Encoder,Decoder};
 use tokio_util::codec::{FramedWrite, FramedRead};
 use tokio::net::TcpStream;
@@ -142,6 +143,40 @@ impl Encoder for MyBytesCodec {
         Ok(())
     }
 }
+pub struct Server;
+
+impl Server {
+    pub async fn run(self, address: String) -> Result<(), Box<dyn Error>> { 
+        println!("Trying to connect to {}", address);
+        let addr = address.as_str().parse::<SocketAddr>()?;
+        let mut listener = TcpListener::bind(&addr).await?; 
+            loop  {
+                println!("Wait for a new socket...");
+                let (mut socket, _) = listener.accept().await?;
+                tokio::spawn(async move {
+                    let (r, w)  = socket.split();
+                    let mut framed_writer = FramedWrite::new(w, MyBytesCodec{});
+                    let mut framed_reader = FramedRead::new(r, MyBytesCodec{});
+    
+                   if let Some(frame) = framed_reader.next().await {
+                        match frame {
+                            Ok(message) => {
+                                let json_parser = JSONParser::new();
+                                println!("{:?}", message);
+                                json_parser.parse(&message);
+                                //let resp: Vec<u8>  = vec![1,2];
+                                //framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
+                          }
+                            Err(e) => {
+                                println!("Error received while we are reading {}", e);
+                            }
+
+                        }
+                    }
+            });
+        }
+    }
+}
 
 pub async fn send(address: String, mesg: String) -> Result<(), Box<dyn Error>> {
     println!("Trying to connect to {}", address);
@@ -154,15 +189,17 @@ pub async fn send(address: String, mesg: String) -> Result<(), Box<dyn Error>> {
     
     let encoded: Vec<u8> = mesg.as_bytes().to_vec();
     framed_writer.send(encoded).await?;
-
+    println!("It is a correct response");
+    /*
     if let Some(frame) = framed_reader.next().await {
         match frame {
             Ok(response) => {
-                let json_parser = JSONParser::new();
+                println!("I got a response");
+                //let json_parser = JSONParser::new();
                 //println!("{:?}", response);
-                json_parser.parse(&response);
-                let resp: Vec<u8>  = vec![1,2];
-                framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
+                //json_parser.parse(&response);
+                //let resp: Vec<u8>  = vec![1,2];
+                //framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
             }
             Err(e) => {
                 println!("Error received while we are reading {}", e);
@@ -170,6 +207,7 @@ pub async fn send(address: String, mesg: String) -> Result<(), Box<dyn Error>> {
 
         }
     }
+    */
     Ok(()) 
 }
 
--- pub_sub_lib/src/main.rs
+++ pub_sub_lib/src/main.rs
@@ -7,52 +7,11 @@ extern crate serde_json;
 extern crate pub_sub;
 
 
-use tokio_util::codec::{FramedWrite, FramedRead};
-use tokio::net::TcpListener;
 use tokio::runtime::Runtime;
-use std::net::SocketAddr;
-use std::error::Error;
-use futures::{SinkExt, StreamExt};
-use pub_sub::{MyBytesCodec, JSONParser};
+use pub_sub::{Server};
 
 
-pub struct Server;
 
-impl Server {
-    async fn run(self, address: String) -> Result<(), Box<dyn Error>> { 
-        println!("Trying to connect to {}", address);
-        let addr = address.as_str().parse::<SocketAddr>()?;
-        let mut listener = TcpListener::bind(&addr).await?; 
-            loop  {
-                println!("Wait for a new socket...");
-                let (mut socket, _) = listener.accept().await?;
-                tokio::spawn(async move {
-                loop {
-                    let (r, w)  = socket.split();
-                    let mut framed_writer = FramedWrite::new(w, MyBytesCodec{});
-                    let mut framed_reader = FramedRead::new(r, MyBytesCodec{});
-    
-                   if let Some(frame) = framed_reader.next().await {
-                        match frame {
-                            Ok(message) => {
-                                let json_parser = JSONParser::new();
-                                println!("{:?}", message);
-                                json_parser.parse(&message);
-                                let resp: Vec<u8>  = vec![1,2];
-                                framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
-                          }
-                            Err(e) => {
-                                println!("Error received while we are reading {}", e);
-                            }
-
-                        }
-                    }
-
-                }
-            });
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut rt = Runtime::new()?;
