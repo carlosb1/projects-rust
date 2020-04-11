23c994a477f970662b0a8dd1b41cecf13d2db928
carlosb <carlos.baezruiz@gmail.com>
2020-04-11 22:39:57 +0200

    Fixed code for server and client

--- pub_sub_lib/src/client.rs
+++ pub_sub_lib/src/client.rs
@@ -1,19 +1,18 @@
-
-
-//type ClientTransport = Framed<TcpStream, MyBytesCodec>;
-use pub_sub::{FactoryMessage,JSONMessage, Message,  send};
-use futures::executor::block_on;
+use pub_sub::{FactoryMessage,JSONMessage,  send};
+use tokio::runtime::Runtime;
 use std::env;
 
 
-pub fn main () {
+pub fn main () -> Result<(), Box<dyn std::error::Error>>{
     let args: Vec<String> = env::args().collect();
     let mut address = "127.0.0.1:12345".to_string();
     if args.len() >= 2 {
         address = args[0].clone();   
     }
+
+    let mut rt = Runtime::new()?;
     let message  = FactoryMessage::ack();
-    let future = send(address, message.to_json().unwrap());
-    block_on(future);
+    let _ = rt.block_on(send(address, message.to_json().unwrap()));
+    Ok(())
 }
 
--- pub_sub_lib/src/main.rs
+++ pub_sub_lib/src/main.rs
@@ -9,6 +9,7 @@ extern crate pub_sub;
 
 use tokio_util::codec::{FramedWrite, FramedRead};
 use tokio::net::TcpListener;
+use tokio::runtime::Runtime;
 use std::net::SocketAddr;
 use std::error::Error;
 use futures::{SinkExt, StreamExt};
@@ -20,8 +21,6 @@ pub struct Server;
 impl Server {
     async fn run(self, address: String) -> Result<(), Box<dyn Error>> { 
         println!("Trying to connect to {}", address);
-        //let remote_address: SocketAddr = address.parse().unwrap();{
-        //let addr = "127.0.0.1:12345".parse::<SocketAddr>()?;
         let addr = address.as_str().parse::<SocketAddr>()?;
         let mut listener = TcpListener::bind(&addr).await?; 
             loop  {
@@ -29,7 +28,6 @@ impl Server {
                 let (mut socket, _) = listener.accept().await?;
                 tokio::spawn(async move {
                 loop {
-                    println!("Running new thread connection");
                     let (r, w)  = socket.split();
                     let mut framed_writer = FramedWrite::new(w, MyBytesCodec{});
                     let mut framed_reader = FramedRead::new(r, MyBytesCodec{});
@@ -56,49 +54,9 @@ impl Server {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let addr = "127.0.0.1:12345";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut rt = Runtime::new()?;
     let server = Server{};
-    server.run(addr.to_string());
-    Ok(())
+    rt.block_on(server.run("127.0.0.1:12345".to_string()))
+
 }
-
-/*
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let addr = "127.0.0.1:12345".parse::<SocketAddr>()?;
-    let mut listener = TcpListener::bind(&addr).await?;
-    loop  {
-            println!("Wait for a new socket...");
-            let (mut socket, _) = listener.accept().await?;
-            tokio::spawn(async move {
-            loop {
-                println!("Running new thread connection");
-                let (r, w)  = socket.split();
-                let mut framed_writer = FramedWrite::new(w, MyBytesCodec{});
-                let mut framed_reader = FramedRead::new(r, MyBytesCodec{});
-
-                if let Some(frame) = framed_reader.next().await {
-                    match frame {
-                        Ok(message) => {
-                            let json_parser = JSONParser::new();
-                            println!("{:?}", message);
-                            json_parser.parse(&message);
-                            let resp: Vec<u8>  = vec![1,2];
-                            framed_writer.send(resp).await.map_err(|e| println!("not response! {}", e)).ok();
-                        }
-                        Err(e) => {
-                            println!("Error received while we are reading {}", e);
-                        }
-
-                    }
-                }
-
-            }
-        
-        });
-    }
-}
-*/
-
