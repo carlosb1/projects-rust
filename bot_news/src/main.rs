67ef39fb5a8f8ae6e6704e1fbd0d43eac5d8986f
carlosb <carlos.baezruiz@gmail.com>
2020-04-26 21:04:41 +0200

    Add code to download from Internet files

--- bot_news/src/main.rs
+++ bot_news/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+extern crate select;
+extern crate reqwest;
+use select::document::Document;
+use select::predicate::{Predicate, Attr, Class, Name};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut res = reqwest::blocking::get("https://www.elpais.com")?;
+    println!("Status = {}", res.status());
+    println!("Headers = {:?}", res.headers());
+    let document = Document::from(res.text()?.as_str());
+    Ok(())
 }
