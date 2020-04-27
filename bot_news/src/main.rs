40eaf5c0670eb2eb4245a9f0e88e4ab339e6ac4e
carlosb <carlos.baezruiz@gmail.com>
2020-04-27 20:45:22 +0200

    Scrapper article

--- bot_news/src/main.rs
+++ bot_news/src/main.rs
@@ -1,12 +1,20 @@
 extern crate select;
 extern crate reqwest;
-use select::document::Document;
-use select::predicate::{Predicate, Attr, Class, Name};
+extern crate scraper;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut res = reqwest::blocking::get("https://www.elpais.com")?;
+use scraper::{Html, Selector};
+
+    fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let res = reqwest::blocking::get("https://elpais.com/tecnologia/2020-04-27/whatsapp-asegura-que-la-medida-para-limitar-el-reenvio-de-mensajes-ha-reducido-la-viralidad-en-un-70.html")?;
     println!("Status = {}", res.status());
     println!("Headers = {:?}", res.headers());
-    let document = Document::from(res.text()?.as_str());
+    
+    let body = res.text().unwrap();
+
+    let fragment = Html::parse_document(&body);
+    let title = fragment.select(&Selector::parse(r#"meta[property="og:title"]"#).unwrap()).next().unwrap().value().attr("content");
+    let description = fragment.select(&Selector::parse(r#"meta[property="og:description"]"#).unwrap()).next().unwrap().value().attr("content");
+    println!("{:?}", title.unwrap());
+    println!("{:?}", description.unwrap());
     Ok(())
 }
