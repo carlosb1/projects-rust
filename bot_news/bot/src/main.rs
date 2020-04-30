fb816458b7e82020e75c8b744b2a278f2da09722
carlosb <carlos.baezruiz@gmail.com>
2020-04-30 23:37:40 +0200

    Added script to connect to meilisearch

--- bot_news/bot/src/main.rs
+++ bot_news/bot/src/main.rs
@@ -2,8 +2,10 @@ use scraper::{Html, Selector};
 use teloxide::prelude::*;
 use url::Url;
 use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hasher};
 
-const ADDRESS_SERVER_NEWS: &str = "http://127.0.0.1:7700/";
+const ADDRESS_SERVER_NEWS: &str = "http://127.0.0.1:7700/indexes/news/documents";
 
 #[tokio::main]
 async fn main(){
@@ -19,7 +21,12 @@ async fn main(){
                 Some(text) => {
                     if is_link(text) {
                         log::info!("We got a link! ");
-                        let _ = download_and_parse(text).await;    
+                        match download_and_parse(text.to_string()).await{
+                            Ok((link, title, description)) => {
+                                    upload_link_info(link.as_str(), title.as_str(), description.as_str()).await;
+                            }
+                            Err(str_e) => {log::error!(" {:}",&str_e);}
+                        };
                     }
                 },
                 None => {},
@@ -39,16 +46,16 @@ fn is_link(text: &str) -> bool{
      }
 }
 
-async fn download_and_parse<'a> (link: &str) -> Result<(), &'a str>{
-    match reqwest::get(link).await{
+async fn download_and_parse<'a> (link: String) -> Result<(String, String, String), &'a str>{
+    match reqwest::get(link.as_str()).await{
         Ok(info) => {
             let body = info.text().await.unwrap_or("fuck you".to_string());
             let fragment = Html::parse_document(&body);
-            let title = fragment.select(&Selector::parse(r#"meta[property="og:title"]"#).unwrap()).next().unwrap().value().attr("content");
-            let description = fragment.select(&Selector::parse(r#"meta[property="og:description"]"#).unwrap()).next().unwrap().value().attr("content");
-            log::info!(" title {:?}", title.unwrap());
-            log::info!(" descrp {:?}", description.unwrap());
-            Ok(())
+            let title = fragment.select(&Selector::parse(r#"meta[property="og:title"]"#).unwrap()).next().unwrap().value().attr("content").unwrap_or("");
+            let description = fragment.select(&Selector::parse(r#"meta[property="og:description"]"#).unwrap()).next().unwrap().value().attr("content").unwrap_or("");
+            log::info!(" title {:?}", title);
+            log::info!(" descrp {:?}", description);
+            Ok((link, title.to_string(), description.to_string()))
         }
         Err(_) => { 
             log::error!("I screwed up");
@@ -60,12 +67,17 @@ async fn download_and_parse<'a> (link: &str) -> Result<(), &'a str>{
 
 
 async fn upload_link_info (link: &str, title: &str, descrip: &str) {
+    log::info!("Uploading link info");
     let mut info_to_upload = HashMap::new();
+    let mut hasher = DefaultHasher::new();
+    hasher.write(link.as_bytes());
+    let id = hasher.finish().to_string();
+    info_to_upload.insert("id", id.as_str());
     info_to_upload.insert("link", link);
     info_to_upload.insert("title", title);
     info_to_upload.insert("description", descrip);
-    
-    let res = reqwest::Client::new().post(ADDRESS_SERVER_NEWS).json(&info_to_upload).send().await;
+    let list_uploads = vec!{info_to_upload};
+    let res = reqwest::Client::new().post(ADDRESS_SERVER_NEWS).json(&list_uploads).send().await;
     match res {
         Ok(_) =>  {log::info!("It was uploaded correctly");}
         Err(e) => {log::info!("{:}", e);}
