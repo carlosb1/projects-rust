4912be932e4ab36abfbd4e8de6ad1c9f8e2916cf
carlosb <carlos.baezruiz@gmail.com>
2020-05-02 13:04:39 +0200

    Added keyword, pending to save in the db

--- bot_news/bot/src/main.rs
+++ bot_news/bot/src/main.rs
@@ -22,8 +22,8 @@ async fn main(){
                     if is_link(text) {
                         log::info!("We got a link! ");
                         match download_and_parse(text.to_string()).await{
-                            Ok((link, title, description)) => {
-                                    upload_link_info(link.as_str(), title.as_str(), description.as_str()).await;
+                            Ok((link, title, description, splitted_keywords)) => {
+                                    upload_link_info(link.as_str(), title.as_str(), description.as_str(), splitted_keywords).await;
                             }
                             Err(str_e) => {log::error!(" {:}",&str_e);}
                         };
@@ -46,16 +46,19 @@ fn is_link(text: &str) -> bool{
      }
 }
 
-async fn download_and_parse<'a> (link: String) -> Result<(String, String, String), &'a str>{
+async fn download_and_parse<'a> (link: String) -> Result<(String, String, String, Vec<String>), &'a str>{
     match reqwest::get(link.as_str()).await{
         Ok(info) => {
             let body = info.text().await.unwrap_or("fuck you".to_string());
             let fragment = Html::parse_document(&body);
             let title = fragment.select(&Selector::parse(r#"meta[property="og:title"]"#).unwrap()).next().unwrap().value().attr("content").unwrap_or("");
             let description = fragment.select(&Selector::parse(r#"meta[property="og:description"]"#).unwrap()).next().unwrap().value().attr("content").unwrap_or("");
+            let keywords = fragment.select(&Selector::parse(r#"meta[name="keywords"]"#).unwrap()).next().unwrap().value().attr("content").unwrap_or("");
+            let splitted_keywords = keywords.split(".").map(|s| s.to_string()).collect();
             log::info!(" title {:?}", title);
             log::info!(" descrp {:?}", description);
-            Ok((link, title.to_string(), description.to_string()))
+            log::info!(" splitted_keywords {:?}", splitted_keywords);
+            Ok((link, title.to_string(), description.to_string(), splitted_keywords))
         }
         Err(_) => { 
             log::error!("I screwed up");
@@ -66,7 +69,7 @@ async fn download_and_parse<'a> (link: String) -> Result<(String, String, String
 }
 
 
-async fn upload_link_info (link: &str, title: &str, descrip: &str) {
+async fn upload_link_info (link: &str, title: &str, descrip: &str, keywords: Vec<String>) {
     log::info!("Uploading link info");
     let mut info_to_upload = HashMap::new();
     let mut hasher = DefaultHasher::new();
@@ -76,6 +79,15 @@ async fn upload_link_info (link: &str, title: &str, descrip: &str) {
     info_to_upload.insert("link", link);
     info_to_upload.insert("title", title);
     info_to_upload.insert("description", descrip);
+    
+    /*
+    let mut count: u8 = 0;
+    for keyword in keywords {
+        let tag = format!("tag{}",count.clone());
+        info_to_upload.insert(tag.as_str(), keyword.as_str());
+        count += 1;
+    }
+    */
     let list_uploads = vec!{info_to_upload};
     let res = reqwest::Client::new().post(ADDRESS_SERVER_NEWS).json(&list_uploads).send().await;
     match res {
