d81631ed8e467165ab86355bc23dd50556fd6026
carlosb <carlos.baezruiz@gmail.com>
2020-05-02 12:11:27 +0200

    Added script to donwload different webs

---
+++ bot_news/scripts/download_web.py
@@ -0,0 +1,35 @@
+import requests
+import shutil
+import sys
+import hashlib
+
+from pathlib import Path
+
+def check_and_download_web(file_to_download: str, output_folder: Path):
+    temp_name = str(hashlib.md5(file_to_download.encode('utf-8')).hexdigest())
+    print(f'file to download: {file_to_download}')
+    print(f'hexdigest hash {temp_name}')
+
+    output_folder.mkdir(parents=True, exist_ok=True)
+    target_path = output_folder / Path(temp_name)
+
+    r = requests.get(file_to_download, stream=True)
+
+    if r.status_code == 200:
+        with open(target_path, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+    print(f'downloaded: {temp_name}')
+
+
+file_to_download = sys.argv[1]
+
+if len(sys.argv) <= 1:
+    print("It is necessary one link to be downloaded")
+    sys.exit()
+
+check_and_download_web(file_to_download, Path('temp'))
+
+
+
