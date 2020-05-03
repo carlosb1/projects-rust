0a9000cc5f350433f56d6f4374531ee278c093f7
carlosb <carlos.baezruiz@gmail.com>
2020-05-03 16:18:43 +0200

    Template for ml project

---
+++ bot_news/ml_news/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ml-news-carlosb",
+    version="0.0.1",
+    author="Carlos B",
+    author_email="",
+    description="ML library to recognize fake news",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+    ]
+)
