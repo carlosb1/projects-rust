3091dfd6af49767ecdc5c1a7afd3662b89e6c7ae
carlosb <carlos.baezruiz@gmail.com>
2020-05-14 19:25:33 +0200

    Added dependencies and corrected app

--- bot_news/ml_endpoint/app.py
+++ bot_news/ml_endpoint/app.py
@@ -22,11 +22,13 @@ output_folder_download = Path('./temp/')
 
 PER_PAGE = 20
 
+
 def get_collection(host: str, port: int):
     connection = MongoClient(host, port)
     db = connection['db_news']
     news = db['news']
     return news
+
 
 ml_bert_analyser = MyBertTransformerSentimentAnalysis()
 
@@ -42,18 +44,20 @@ app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
 cors = CORS(app, resources={r"/api/*": {"origins": '*'}})
 app.config['CORS_HEADER'] = 'Content-Type'
 
+
 def pagination(news, page: int, size: int):
     paginated = []
-    for entry in news.find().skip((page-1)*size).limit(size):
+    for entry in news.find().skip((page - 1) * size).limit(size):
         entry["_id"] = str(entry["_id"])
         paginated.append(entry)
     return paginated
 
+
 @app.route('/api/news', methods=['GET'])
 @cross_origin()
 def get_news():
-    page = request.args.get('page',1, type=int)
-    size = request.args.get('size',PER_PAGE, type=int)
+    page = request.args.get('page', 1, type=int)
+    size = request.args.get('size', PER_PAGE, type=int)
     # set up  db connection
     app.logger.info("Receiving get query")
     news = get_collection(mongo_host, mongo_port)
@@ -64,7 +68,7 @@ def get_news():
 @cross_origin()
 def get_one_news(_id: str):
     news = get_collection(mongo_host, mongo_port)
-    found_one = news.find_one({"id":_id})
+    found_one = news.find_one({"id": _id})
     if not found_one:
         return jsonify({})
     if '_id' in found_one:
@@ -77,8 +81,6 @@ def get_one_news(_id: str):
 @cross_origin()
 def post_news():
     # set up  db connection
-    news = get_collection(mongo_host, mongo_port)
-
     app.logger.info("Receiving post query")
     content = request.json
 
@@ -88,29 +90,40 @@ def post_news():
 
     list_ids = []
     for entry in content:
-        if type(entry) is not dict and not (all(elem in ['id', 'link', 'title', 'description']) for elem in entry):
+        if type(entry) is not dict and not (all(
+                elem in ['id', 'link', 'title', 'description'])
+                                            for elem in entry):
             app.logger.info(f'Content doesn include enough info {str(entry)}')
             continue
         app.logger.info("-----------------")
         app.logger.info(f'{str(entry)}')
         app.logger.info("-----------------")
-        run_batch.apply_async((entry['id'], entry['link'], entry['title'], entry['description']))
+        run_batch.apply_async(
+            (entry['id'], entry['link'], entry['title'], entry['description']))
         list_ids.append(entry['id'])
 
     return factory_responses.new201({'ids': list_ids})
 
+
 @celery.task
 def run_batch(_id: str, link: str, title: str, description: str):
-        app.logger.info(f'Executing analysed batch task {str(link)}')
-        # set up  db connection
-        news = get_collection(mongo_host, mongo_port)
+    app.logger.info(f'Executing analysed batch task {str(link)}')
+    # set up  db connection
+    news = get_collection(mongo_host, mongo_port)
 
-        # TODO ADD ML ALGORITHM
-        data_sentiment = ml_bert_analyser.run(description)
-        key = {'id': _id}
-        data = {'id': _id, 'link': link, 'title': title, 'description': description, 'data_ml': data_sentiment}
-        news.update(key, data, upsert=True)
-        check_and_download_web(link, output_folder_download)
+    # TODO ADD ML ALGORITHM
+    data_sentiment = ml_bert_analyser.run(description)
+    key = {'id': _id}
+    data = {
+        'id': _id,
+        'link': link,
+        'title': title,
+        'description': description,
+        'data_ml': data_sentiment
+    }
+    news.update(key, data, upsert=True)
+    check_and_download_web(link, output_folder_download)
+
 
 if __name__ == '__main__':
     host = '0.0.0.0'
@@ -124,7 +137,7 @@ if __name__ == '__main__':
     celery_argvs = ['worker', '--loglevel=DEBUG']
     import threading
     celery_thread = threading.Thread(target=celery.worker_main,
-                                       args=[celery_argvs])
+                                     args=[celery_argvs])
     celery_thread.start()
     app.logger.info("Running API REST")
     app.run(host=host, port=port, debug=debug)
