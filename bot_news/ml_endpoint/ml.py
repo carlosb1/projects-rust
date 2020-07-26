55a180f76ffd5464a2f3351cfe49f2727e2bc151
Carlos B <mcflurry0@gmail.com>
2020-07-26 19:43:51 +0200

    Fix dockerfile config

--- bot_news/ml_endpoint/ml.py
+++ bot_news/ml_endpoint/ml.py
@@ -3,7 +3,6 @@ from transformers import AutoModelForSequenceClassification
 from transformers import TextClassificationPipeline
 
 
-
 SENTIMENT_MODEL = 'nlptown/bert-base-multilingual-uncased-sentiment'
 
 
@@ -11,8 +10,8 @@ class MyBertTransformerSentimentAnalysis():
     def __init__(self, name_model: str = SENTIMENT_MODEL):
         tokenizer = AutoTokenizer.from_pretrained(name_model)
         model = AutoModelForSequenceClassification.from_pretrained(name_model)
-        self.pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer)
-
+        self.pipe = TextClassificationPipeline(
+            model=model, tokenizer=tokenizer)
 
     def run(self, input_data: str) -> dict:
         output = self.pipe(input_data)
