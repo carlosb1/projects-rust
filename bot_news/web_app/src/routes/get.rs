0383132bdaee20c2d5551b4b5f87f29f301e35ed
Carlos B <mcflurry0@gmail.com>
2020-09-08 00:24:40 +0200

    Added first example applying tera templates

--- bot_news/web_app/src/routes/get.rs
+++ bot_news/web_app/src/routes/get.rs
@@ -1,9 +1,10 @@
 #![feature(proc_macro_hygiene)]
 
-use std::io;
-use rocket::response::{NamedFile};
+use rocket::response::NamedFile;
 use rocket_contrib::json::{Json, JsonValue};
-
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+use std::io;
 
 #[derive(Debug)]
 pub enum ApiError {
@@ -19,4 +20,10 @@ pub fn index() -> io::Result<NamedFile> {
 #[get("/share")]
 pub fn single_page_app() -> io::Result<NamedFile> {
     NamedFile::open("static/build/index.html")
+}
+
+#[get("/main")]
+pub fn main() -> Template {
+    let context: HashMap<&str, &str> = [("name", "Carlos")].iter().cloned().collect();
+    Template::render("main", &context)
 }
