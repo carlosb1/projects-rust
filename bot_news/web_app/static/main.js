23c5f03948f75cb5e3d24c5e1da4072bdf86acef
Carlos B <mcflurry0@gmail.com>
2020-09-10 20:46:01 +0200

    Added minimal example to use acios

--- bot_news/web_app/static/main.js
+++ bot_news/web_app/static/main.js
@@ -1,16 +1,28 @@
-function new_comment(comment) {
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:8000';
+
+export function new_comment(comment) {
 	console.log('Adding a new comment: ' + comment);
 }
 
-function like(userid, articleid) {
+export function like(userid, articleid) {
 	console.log('User id:'+userid+" article:"+articleid);
 }
 
-function approve(userid, articleid) {
+export function approve(userid, articleid) {
 	console.log('approve!');
 }
 
-function fake(userid, articleid)  {
+export function fake(userid, articleid)  {
 	console.log('faked');
+	axios.put('/fake',{
+		'id_user': '1',
+		'id_article': '2'
+	}).then(function (response) {
+		console.log(response);
+	}).catch(function(error) {
+		console.log(error);
+	});
 }
 
