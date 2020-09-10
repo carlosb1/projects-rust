b338885aba51dce0b470270cf869dc6c1d576f34
Carlos B <mcflurry0@gmail.com>
2020-09-10 02:23:20 +0200

    Added enable buttons

--- bot_news/web_app/static/main.js
+++ bot_news/web_app/static/main.js
@@ -1,1 +1,16 @@
-alert("Replace me with main.js from the React Single Page App later")
+function new_comment(comment) {
+	console.log('Adding a new comment: ' + comment);
+}
+
+function like(userid, articleid) {
+	console.log('User id:'+userid+" article:"+articleid);
+}
+
+function approve(userid, articleid) {
+	console.log('approve!');
+}
+
+function fake(userid, articleid)  {
+	console.log('faked');
+}
+
