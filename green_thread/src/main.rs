f38da3042a9b1e682bed0886bae791c5da084632
carlosb1 <mcflurry0@gmail.com>
2021-09-19 22:46:20 +0200

    Added function for print stack

--- green_thread/src/main.rs
+++ green_thread/src/main.rs
@@ -34,6 +34,15 @@ fn main() {
         let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
         std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
         ctx.rsp = sb_aligned.offset(-16) as u64;
+
+        let stack_ptr = stack.as_mut_ptr();
+        for i in (0..SSIZE).rev() {
+            println!(
+                "mem {}, val: {}",
+                stack_ptr.offset(i as isize) as usize,
+                *stack_ptr.offset(i as isize)
+            )
+        }
         gt_switch(&mut ctx);
     }
 }
