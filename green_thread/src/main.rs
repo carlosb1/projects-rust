49cd6a061fe0052938d319ec756688393a61493f
carlosb1 <mcflurry0@gmail.com>
2021-09-19 21:43:48 +0200

    Example for running green thread.

--- green_thread/src/main.rs
+++ green_thread/src/main.rs
@@ -8,6 +8,32 @@ struct ThreadContext {
     rsp: u64,
 }
 
+fn hello() -> ! {
+    println!("I LOVE WAKING UP ON A NEW STACK!");
+    loop {}
+}
+
+unsafe fn gt_switch(new: *const ThreadContext) {
+    llvm_asm!(
+    "
+        mov 0x00($0), %rsp
+        ret
+        "
+    :
+    : "r"(new)
+    :
+    : "alignstack" // it will work without this now, will need it later
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut ctx = ThreadContext::default();
+    let mut stack = vec![0_u8; SSIZE as usize];
+    unsafe {
+        let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
+        let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
+        std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
+        ctx.rsp = sb_aligned.offset(-16) as u64;
+        gt_switch(&mut ctx);
+    }
 }
