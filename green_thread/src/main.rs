f7af04406fbf9d9eb8d684c8b48d5161580742eb
carlosb1 <mcflurry0@gmail.com>
2021-10-10 21:12:04 +0200

    Updated for customized scheduler

--- green_thread/src/main.rs
+++ green_thread/src/main.rs
@@ -37,6 +37,8 @@ struct Thread {
     ctx: ThreadContext,
     state: State,
 }
+
+//Nopush() on the vector or any other methods that might trigger a reallocation
 impl Thread {
     fn new(id: usize) -> Self {
         Thread {
@@ -45,6 +47,99 @@ impl Thread {
             ctx: ThreadContext::default(),
             state: State::Available,
         }
+    }
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        let base_thread = Thread {
+            id: 0,
+            stack: vec![0_u8; DEFAULT_STACK_SIZE],
+            ctx: ThreadContext::default(),
+            state: State::Running,
+        };
+
+        let mut threads = vec![base_thread];
+        let mut available_threads: Vec<Thread> = (1..MAX_THREADS).map(|i| Thread::new(i)).collect();
+        threads.append(&mut available_threads);
+
+        Runtime {
+            threads,
+            current: 0,
+        }
+    }
+
+    /// This is cheating a bit, but we need a pointer to our Runtime
+    /// stored so we can call yield on it even if we don't have a     
+    /// reference to it.
+    pub fn init(&self) {
+        unsafe {
+            let r_ptr: *const Runtime = self;
+            RUNTIME = r_ptr as usize;
+        }
+    }
+
+    pub fn run(&mut self) {
+        while self.t_yield() {}
+        std::process::exit(0);
+    }
+
+    fn t_return(&mut self) {
+        if self.current != 0 {
+            self.threads[self.current].state = State::Available;
+            self.t_yield();
+        }
+    }
+    fn t_yield(&mut self) -> bool {
+        let mut pos = self.current;
+        while self.threads[pos].state != State::Ready {
+            pos += 1;
+            if pos == self.threads.len() {
+                pos = 0;
+            }
+            if pos == self.current {
+                return false;
+            }
+        }
+
+        if self.threads[self.current].state != State::Available {
+            self.threads[self.current].state = State::Ready;
+        }
+        self.threads[pos].state = State::Running;
+        let old_pos = self.current;
+        self.current = pos;
+
+        unsafe {
+            let old: *mut ThreadContext = &mut self.threads[old_pos].ctx;
+            let new: *const ThreadContext = &self.threads[pos].ctx;
+            llvm_asm!(
+            "mov $0, %rdi
+             mov $1, %rdi"::"r"(old), "r"(new)
+            );
+            switch();
+        }
+        //  prevent the compiler from optimizing our code away.
+        self.threads.len() > 0
+    }
+
+    fn switch(&mut self) {}
+
+    pub fn spawn(&mut self, f: fn()) {
+        let available = self
+            .threads
+            .iter_mut()
+            .find(|t| t.state == State::Available)
+            .expect("no available thread");
+        let size = available.stack.len();
+        unsafe {
+            let s_ptr = available.stack.as_mut_ptr().offset(size as isize);
+            let s_ptr = (s_ptr as usize & !15) as *mut u8;
+            std::ptr::write(s_ptr.offset(-16) as *mut u64, guard as u64);
+            std::ptr::write(s_ptr.offset(-24) as *mut u64, skip as u64);
+            std::ptr::write(s_ptr.offset(-32) as *mut u64, f as u64);
+            available.ctx.rsp = s_ptr.offset(-32) as u64;
+        }
+        available.state = State::Ready;
     }
 }
 
