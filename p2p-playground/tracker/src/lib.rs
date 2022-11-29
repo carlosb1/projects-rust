9dd26d3c24d3ca7af32d9822de91477be34e02ba
carlosb1 <mcflurry0@gmail.com>
2022-11-29 22:57:27 +0100

    First domain operation
    Signed-off-by: carlosb1 <mcflurry0@gmail.com>

--- p2p-playground/tracker/src/lib.rs
+++ p2p-playground/tracker/src/lib.rs
@@ -1,3 +1,4 @@
+mod application;
 mod client;
 mod message;
 
@@ -12,7 +13,6 @@ use std::error::Error;
 use std::io;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio_util::codec::{Decoder, Encoder};
 use tokio_util::codec::{FramedRead, FramedWrite};
@@ -141,36 +141,5 @@ impl Server {
                 }
             });
         }
-    }
-}
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
     }
 }
