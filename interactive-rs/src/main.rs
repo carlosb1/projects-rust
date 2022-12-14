8768244bfbfca7d645c60ea0296306b87caf74e8
carlosb1 <mcflurry0@gmail.com>
2022-12-14 23:12:19 +0100

    First example with design doc

--- interactive-rs/src/main.rs
+++ interactive-rs/src/main.rs
@@ -1,52 +1,17 @@
 extern crate termion;
 
-use std::io::{stdin, stdout, Write};
-use termion::cursor::DetectCursorPos;
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    link: String,
+    tags: Vec<String>,
+}
 
 fn main() {
-    // Get the standard input stream.
-    let stdin = stdin();
-    // Get the standard output stream and go to raw mode.
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    let args = Cli::parse();
 
-    write!(
-        stdout,
-        "{}{}q to exit. Type stuff, use alt, and so on.{}",
-        // Clear the screen.
-        termion::clear::All,
-        // Goto (1,1).
-        termion::cursor::Goto(1, 1),
-        // Hide the cursor.
-        termion::cursor::Hide
-    )
-    .unwrap();
-    // Flush stdout (i.e. make the output appear).
-    stdout.flush().unwrap();
-
-    for c in stdin.keys() {
-        let val = c.unwrap();
-
-        if let Key::Char('q') = val {
-            break;
-        }
-        if let Key::Char('\n') = val {
-            let (x, y) = stdout.cursor_pos().unwrap();
-            write!(stdout, "{}{}", " ", termion::cursor::Goto(0, y + 1)).unwrap();
-            continue;
-        }
-        if let Key::Char(x) = val {
-            //stdout.write_all(x.to_string().as_bytes()).unwrap();
-            //print!("{:}", x);
-            write!(stdout, "{}", x).unwrap();
-        }
-
-        // Flush again.
-        stdout.flush().unwrap();
-    }
-
-    // Show the cursor again before we exit.
-    write!(stdout, "{}", termion::cursor::Show).unwrap();
+    println!("{:?}", args.link);
+    println!("{:?}", args.tags);
 }
