2b9f664f6716d595c68304a89ad86359b18f2fa3
carlosb1 <mcflurry0@gmail.com>
2022-12-15 23:12:13 +0100

    Added repo for saving links
    Signed-off-by: carlosb1 <mcflurry0@gmail.com>

--- interactive-rs/src/main.rs
+++ interactive-rs/src/main.rs
@@ -2,6 +2,20 @@ extern crate termion;
 
 use clap::Parser;
 
+struct DataRepository {
+    file_path: String,
+}
+impl DataRepository {
+    pub fn add(self, link: String, tags: Vec<String>) {
+        let tree = sled::open(self.file_path.to_string())
+            .map_err(|x| format!("Error: {x}").to_string())
+            .expect(format!("It was not possible open the file {:?}", self.file_path).as_str());
+        tags.iter().for_each(|tag| {
+            let _ = tree.insert(tag.as_str(), link.as_str());
+        });
+    }
+}
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -12,6 +26,8 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    println!("{:?}", args.link);
-    println!("{:?}", args.tags);
+    let repo = DataRepository {
+        file_path: "data.db".to_string(),
+    };
+    repo.add(args.link, args.tags);
 }
